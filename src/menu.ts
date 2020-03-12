da8c1b42ae8c1dbbc4af8ac3bfb6b4335aeafb18
jamesjianpeng@gmail.com <pengjian>
2020-03-12 15:09:37 +0800

    perf: 修改注释，添加 article add html 属性

--- src/menu.ts
+++ src/menu.ts
@@ -1,10 +1,10 @@
 /**
  * @interface 菜单
- * @param icon 菜单小图标
- * @param title 菜单名字
- * @param path 菜单路由
- * @param children 子菜单
- * @param version 版本 
+ * @property icon 菜单小图标
+ * @property title 菜单名字
+ * @property path 菜单路由
+ * @property children 子菜单
+ * @property version 版本
  */
 export interface IMenu {
     icon?: string
--- src/tag.ts
+++ src/tag.ts
@@ -2,12 +2,12 @@ import { IBase } from './base'
 
 /**
  * @interface 标签
- * @param text 标签名字
- * @param value 标签key
- * @param createTime 文章创建时间 YYYY-MM-DD HH:mm:ss
- * @param updateTime 文章更新时间 YYYY-MM-DD HH:mm:ss
- * @param version 版本
- * @param history 修改历史
+ * @property text 标签名字
+ * @property value 标签key
+ * @property createTime 文章创建时间 YYYY-MM-DD HH:mm:ss
+ * @property updateTime 文章更新时间 YYYY-MM-DD HH:mm:ss
+ * @property version 版本
+ * @property history 修改历史
  */
 export interface ITag extends IBase {
     text: string
